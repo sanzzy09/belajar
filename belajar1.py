ee984d36c05717c212d78fc3b60c8a66a89d0110
Sanzzy <ikhsan12636@gmail.com>
2026-02-16 18:23:45 +0700

    Create belajar1.py

---
+++ belajar1.py
@@ -0,0 +1,7 @@
+# Belajar Python-1
+
+a = 'Belajar Python Bagian 1' # Variable
+b = a; # Menggunakan Variable Isi Variable Diatas Untukk Variable Baru
+print(1+1) # Penjumlahan (+) Pengurangan (-) Pembagian (/) Perkalian (*) Pangkat (**) Tipe Data ( Integer )
+print(a) # Print Variable Diatas
+print('Hello World') # Print Text ( String )
